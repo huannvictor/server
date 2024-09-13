6a240e4c764034cd575068a540f1b6cac5abe130
Huann <huannvictor@gmail.com>
2024-09-13 10:56:40 -0300

    feat: refactored routes

---
+++ src/http/routes/create-completions.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { CreateGoalCompletion } from '../../features/create-goal-completions'
+
+export const createCompletionRoute: FastifyPluginAsyncZod = async app => {
+  app.post(
+    '/completions',
+    {
+      schema: {
+        body: z.object({ goalId: z.string() }),
+      },
+    },
+    async request => {
+      const { goalId } = request.body
+      await CreateGoalCompletion({ goalId })
+    }
+  )
+}
